1b644b08c51b4b692d33d5f466df16ec4b937e71
TadeuTorres <marcos.torres@GESINFHZ1>
2017-05-25 10:42:07 -0300

    Adicionando projetos iniciais

---
+++ Clients/Constants/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constants
+{
+    public static class Constants
+    {
+        public const string BaseAddress = "https://developers.es.gov.br/acessocidadao/is";
+
+        public const string AuthorizeEndpoint = BaseAddress + "/connect/authorize";
+        public const string LogoutEndpoint = BaseAddress + "/connect/endsession";
+        public const string TokenEndpoint = BaseAddress + "/connect/token";
+        public const string UserInfoEndpoint = BaseAddress + "/connect/userinfo";
+        public const string IdentityTokenValidationEndpoint = BaseAddress + "/connect/identitytokenvalidation";
+
+        //public const string AspNetWebApiSampleApi = "http://localhost:2727/";
+    }
+}
---
+++ Clients/MVC.Owin.Implicit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC.Owin.Implicit.MvcApplication" Language="C#" %>
