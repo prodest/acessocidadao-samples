1dee9dd3f224504c8d9c675be26edc90ec069f84
TadeuTorres <marcos.torres@GESINFHZ1>
2017-05-25 10:44:08 -0300

    Configurando constantes

--- Clients/Constants/Constants.cs
+++ Clients/Constants/Constants.cs
@@ -12,6 +12,7 @@ namespace Constants
         public const string UserInfoEndpoint = BaseAddress + "/connect/userinfo";
         public const string IdentityTokenValidationEndpoint = BaseAddress + "/connect/identitytokenvalidation";
 
-        //public const string AspNetWebApiSampleApi = "http://localhost:2727/";
+        public const string ClientId = "";
+        public const string ClientSecret = "";
     }
 }
